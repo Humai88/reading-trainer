a555d4feac6388244f5056d10785e5438f263f69
Gumay <humaivier88@gmail.com>
2021-10-20 17:50:52 +0100

    Add touchStart event

--- src/components/Dnd/Droppable/index.tsx
+++ src/components/Dnd/Droppable/index.tsx
@@ -10,12 +10,22 @@ export const Droppable: React.FC<DroppablePropsType> = ({
     const data = e.dataTransfer.getData("transfer");
     (e.target as HTMLDivElement).appendChild(document.getElementById(data)!);
   };
+  const dropMob = (e: React.TouchEvent<HTMLDivElement>) => {
+    e.preventDefault();
+  };
+
   const allowDrop = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
   const finalClasses = `${className ? className : ""}`;
   return (
-    <div id={id} onDrop={drop} onDragOver={allowDrop} className={finalClasses}>
+    <div
+      id={id}
+      onTouchStart={dropMob}
+      onDrop={drop}
+      onDragOver={allowDrop}
+      className={finalClasses}
+    >
       {children}
     </div>
   );
